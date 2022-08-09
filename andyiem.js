d32f47f0d84dabad5243df0b50cf9a661ebf1f43
Vmazed <andyiem123@gmail.com>
2022-08-08 23:19:51 -0500

    first commit

---
+++ andyiem.js
@@ -0,0 +1,18 @@
+// Variables
+//let , const, var
+
+//Example
+// let a = 10
+// console.log(`The initial value of a is: 
+//if/else conditionals
+//if: if this then do that
+//if*(conditional)
+
+function bar(age){
+    if(age>=21){
+        return `You are old enough for the bar you can enter`
+    }
+}
+console.log(bar(22))
+
+
